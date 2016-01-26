21ed951c63171f32f38967159dd4dae6af94a8ad
Nils Philippsen <nils@redhat.com>
2016-01-26 17:27:39 +0100

    NONWORKING: convert memcache role to RoleSetting API

--- config/roles/memcache/role.py
+++ config/roles/memcache/role.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2015 Red Hat, Inc.
+# Copyright (C) 2015, 2016 Red Hat, Inc.
 #
 # Authors:
 # Stephen Gallagher <sgallagh@redhat.com>
+# Nils Philippsen <nils@redhat.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,15 +23,15 @@
 # It is deployed inside of a Docker container
 
 import os
-import dbus.service
 from rolekit.server.rolebase import RoleBase
 from rolekit.server.rolebase import RoleDeploymentValues
 from rolekit import async
 from rolekit.dbus_utils import SystemdJobHandler
 from rolekit.config import SYSTEMD_UNITS
-from rolekit.errors import COMMAND_FAILED, INVALID_VALUE, INVALID_PROPERTY
+from rolekit.errors import COMMAND_FAILED
 from rolekit.errors import RolekitError
 from rolekit.logger import log
+from rolekit.server.settings import RoleSetting
 from rolekit.server.io.systemd import enable_units
 from rolekit.server.io.systemd import SystemdContainerServiceUnit
 
@@ -43,111 +44,103 @@ MiB_SIZE = 1024 * 1024
 GiB_SIZE = MiB_SIZE * 1024
 
 
+def _cache_size_default():
+    # Do a late import of psutil. This will only get
+    # used during a deployment, so we don't need to
+    # have it as a dependency for rolekit itself
+    import psutil
+
+    # Get the total number of bytes in local system memory
+    total_ram = psutil.virtual_memory().total
+
+    # If 25% of the available memory is less than 1GB, use
+    # that for the cache.
+    return int(min(total_ram / 4 / MiB_SIZE, GiB_SIZE / MiB_SIZE))
+
+def _cache_size_constraint(cache_size):
+    # Do a late import of psutil. This will only get
+    # used during a deployment, so we don't need to
+    # have it as a dependency for rolekit itself
+    import psutil
+
+    return cache_size < psutil.virtual_memory().total / MiB_SIZE
+
+def _threads_constraint(threads):
+    # Up to four threads should be safe on any platform
+    # More than that should be limited by the available CPUs
+    return 0 < threads <= max(4, os.cpu_count())
+
+
 class Role(RoleBase):
-    # Use _DEFAULTS from RoleBase and overwrite settings or add new if needed.
-    # Without overwrites or new settings, this can be omitted.
-    _DEFAULTS = dict(RoleBase._DEFAULTS, **{
-        # All roles must provide the following four options:
-        # version, services, packages and firewall
 
-        # Version of the *role* (not the services it provides)
-        "version": 1,
+    ### The following properties/settings are defined/set up in RoleBase.
+    ### Role-specific (default) values are set here.
 
-        # A list of systemd services that must be started with
-        # this role.
-        "services": [ ],
+    # version of the *role* (not the services it provides)
+    version = 1
 
-        # A list of packages that must be installed by the
-        # package manager to be able to deploy and run this
-        # role. These will be installed before the deploy()
-        # routine is invoked, so it can contain packages
-        # needed for deployment as well as runtime.
-        "packages": [ "memcached",
-                      "docker",
-                      "python3-docker-py",
-                      "python3-psutil" ],
+    # A list of systemd services that must be started with
+    # this role. Empty list by default, so no need to set it.
+    # services = []
 
-        # The ports or "services" that need to be available
-        # in the firewall.
-        # These will be opened automatically as part of
-        # deployment and associated with the default
-        # firewall zone of the system.
+    # A list of packages that must be installed by the package manager to be
+    # able to deploy and run this role. These will be installed before the
+    # role-specific deployment takes place, so it can contain packages needed
+    # for deployment as well as runtime.
+    packages = ['memcached', 'docker', 'python3-docker-py', 'python3-psutil']
 
-        "firewall": { "ports": [ '%s/tcp' % MEMCACHED_DEFAULT_PORT,
-                                 '%s/udp' % MEMCACHED_DEFAULT_PORT],
-                      "services": [ ] },
-
-
-        # Role-specific settings belong here, with their defaults
-        # Roles that have no default should be specified here, with
-        # 'None' as their default
-
-        # How many megabytes to allocate for the cache
-        # If this is unspecified, the default will be 1 GB or
-        # 25% of the total RAM on the system, whichever is smaller
-        "cache_size": GiB_SIZE / MiB_SIZE,
-
-        # How many concurrent connections are allowed?
-        # Default: 1024 (from upstream recommendations)
-        "connections": 1024,
-
-        # How many threads should memcache run?
-        # Upstream does not recommend changing this value from the
-        # default.
-        "threads": 4,
-    })
+    # The 'ports' and/or 'services' will be opened automatically as part of
+    # deployment and associated with the default firewall zone of the system.
+    # Ports can be single ports or ranges and are formatted as
+    # "<port>[-<endport>]/<protocol>", services are the names of services in
+    # firewalld.
+    firewall = {'ports': [
+        "{}/{}".format(MEMCACHED_DEFAULT_PORT, proto)
+        for proto in ('tcp', 'udp')]}
 
     # Maximum number of instances of this role that can be instantiated
     # on a single host.
 
     # Until we work out how to set multiple firewall ports, this will
     # provide a single instance.
-    _MAX_INSTANCES = 1
+    max_instances = 1
 
+    ### These settings are specific to memcached.
+
+    # RoleSetting supports callables for computed defaults.
+    cache_size = RoleSetting(
+        int, default=_cache_size_default, constraint=_cache_size_constraint,
+        help="How many megabytes to allocate for the cache. If this is "
+             "unspecified, the default will be 1 GB or 25% of the total RAM "
+             "on the system, whichever is smaller.")
+
+    # How many concurrent connections are allowed?
+    # Default: 1024 (from upstream recommendations)
+    connections = RoleSetting(
+        int, default=1024, constraint=lambda x: 0 < x <= 65536,
+        help="Maximum number of concurrent connections.")
+
+    threads = RoleSetting(
+        int, default=4, constraint=_threads_constraint,
+        help="Number of threads used for memcached. "
+             "It is not recommended to change this value.")
 
     # Initialize role
-    def __init__(self, name, directory, *args, **kwargs):
-        # Get the default initialization from the RoleBase class
-        # Always use this.
-        super(Role, self).__init__(name, directory, *args, **kwargs)
-
-        # Role-specific initialization goes here, if any
+    # def __init__(self, name, directory, *args, **kwargs):
+    #     # Always invoke the base class constructor if overriding __init__()
+    #     super(Role, self).__init__(name, directory, *args, **kwargs)
+    #
+    #     # Role-specific initialization goes here, if any
+    #     ...
 
 
     # Deploy code
-    def do_deploy_async(self, values, sender=None):
+    def do_deploy_async(self, sender=None):
         log.debug9("TRACE: do_deploy_async")
         # Run whatever series of actions are needed to deploy
         # this role in a meaningful way.
         #
         import docker
-
-        # Get the default cache size
-        # Find out how much RAM is available on the system
-        if 'cache_size' not in values:
-            # Do a late import of psutil. This will only get
-            # used during a deployment, so we don't need to
-            # have it as a dependency for rolekit itself
-            import psutil
-
-            # Get the total number of bytes in local system memory
-            total_ram = psutil.virtual_memory().total
-
-            # If 25% of the available memory is less than 1GB, use
-            # that for the cache.
-            if total_ram / 4 < GiB_SIZE:
-                # Set cache_size in MiB
-                values['cache_size'] = int(total_ram / 4 / MiB_SIZE)
-            else:
-                # Cap the default size at 1 GB in MiB
-                values['cache_size'] = int(GiB_SIZE / MiB_SIZE)
-
-        # Set defaults
-        if "connections" not in values:
-            values["connections"] = self._DEFAULTS["connections"]
-
-        if "threads" not in values:
-            values["threads"] = self._DEFAULTS["threads"]
 
         # Create a container for memcached and launch that
         log.debug2("Enabling the Docker container manager")
@@ -157,17 +150,24 @@ class Role(RoleBase):
 
         log.debug2("Starting the Docker container manager")
         with SystemdJobHandler() as job_handler:
-            job_path = job_handler.manager.StartUnit("docker.service", "replace")
+            job_path = job_handler.manager.StartUnit(
+                    "docker.service", "replace")
             job_handler.register_job(job_path)
 
             job_results = yield job_handler.all_jobs_done_future()
-            if any([x for x in job_results.values() if x not in ("skipped", "done")]):
-                details = ", ".join(["%s: %s" % item for item in job_results.items()])
-                raise RolekitError(COMMAND_FAILED, "Starting docker.service failed: %s" % details)
+            if any(
+                    x for x in job_results.values()
+                    if x not in ("skipped", "done")):
+                details = ", ".join(
+                    "{}: {}".format(*item) for item in job_results.items())
+                raise RolekitError(
+                    COMMAND_FAILED,
+                    "Starting docker.service failed: {}".format(details))
 
-        log.debug2("Pulling %s image from Docker Hub" % MEMCACHED_DOCKER_IMAGE)
-        dockerclient = docker.Client(base_url=docker.utils.utils.DEFAULT_UNIX_SOCKET,
-                                     version='auto')
+        log.debug2("Pulling {} image from Docker Hub".format(
+            MEMCACHED_DOCKER_IMAGE))
+        dockerclient = docker.Client(
+                base_url=docker.utils.utils.DEFAULT_UNIX_SOCKET, version='auto')
 
         # First, pull down the latest version of the memcached container
         dockerclient.pull(MEMCACHED_DOCKER_IMAGE, tag="latest")
@@ -176,12 +176,12 @@ class Role(RoleBase):
         # Generate a systemd service unit for this container
         container_unit = SystemdContainerServiceUnit(
             image_name = MEMCACHED_DOCKER_IMAGE,
-            container_name = "memcached_%s" % self.get_name(),
-            desc="memcached docker container - %s" % self.get_name(),
+            container_name = "memcached_{}".format(self.name),
+            desc="memcached docker container - {}".format(self.name),
             env = {
-                "MEMCACHED_CACHE_SIZE": str(values['cache_size']),
-                "MEMCACHED_CONNECTIONS": str(values['connections']),
-                "MEMCACHED_THREADS": str(values['threads'])
+                "MEMCACHED_CACHE_SIZE": str(self.cache_size),
+                "MEMCACHED_CONNECTIONS": str(self.connections),
+                "MEMCACHED_THREADS": str(self.threads)
             },
             ports = ("{0}:{0}/tcp".format(MEMCACHED_DEFAULT_PORT),
                      "{0}:{0}/udp".format(MEMCACHED_DEFAULT_PORT))
@@ -196,28 +196,27 @@ class Role(RoleBase):
         # Return the target information
         target = RoleDeploymentValues(self.get_type(), self.get_name(),
                                       "Memory Cache")
-        target.add_required_units(['memcached_%s.service' % self.get_name()])
+        target.add_required_units(['memcached_{}.service'.format(self.name)])
 
         log.debug9("TRACE: exiting do_deploy_async")
         yield target
 
     # Redeploy code
-    def do_redeploy_async(self, values, sender=None):
+    def do_redeploy_async(self, sender=None):
         # Run whatever series of actions are needed to update the
         # role with a new high-level configuration.
         # Note: This should be configuration of the role itself,
         # not configuration of data held by the role. That should
         # be managed by the standard tools for interacting with
         # the role.
-        #
 
         # For this role, we can just run the decommission routine
         # and then the deploy routine again.
-        yield async.call_future(self.do_decommission_async(values, sender))
+        yield async.call_future(self.do_decommission_async(sender))
 
         # Invoke the deploy routine again
         # Discard the target return; we don't need it
-        yield async.call_future(self.do_deploy_async(values, sender))
+        yield async.call_future(self.do_deploy_async(sender))
 
         # Success
         yield None
@@ -228,7 +227,7 @@ class Role(RoleBase):
         # Remove the container unit
         # Nothing else needs to happen here; the image is
         # removed as part of the role stop() operation
-        path = "%s/memcached_%s.service" % (SYSTEMD_UNITS, self.get_name())
+        path = "{}/memcached_{}.service".format(SYSTEMD_UNITS, self.name)
         try:
             os.unlink(path)
         except FileNotFoundError:
@@ -250,74 +249,3 @@ class Role(RoleBase):
 
         # Remove this line for real roles
         raise NotImplementedError()
-
-
-
-    # Check own properties
-    def do_check_property(self, prop, value):
-        # All options passed to the role must be validated
-        # At minimum, this routine should call one of the
-        # following routines for all known settings:
-        #  * self.check_type_bool(value)
-        #  * self.check_type_dict(value)
-        #  * self.check_type_int(value)
-        #  * self.check_type_list(value)
-        #  * self.check_type_string(value)
-        #  * self.check_type_string_list(value)
-        # Each of these routines will return True if
-        # the value is appropriate or raise a
-        # RolekitError if it is not.
-        # If you wish to add your own checks, this
-        # function must return as follows:
-        # * True: The value passes all validation
-        # * False: The setting was unknown to this role
-        # * RolekitError: The value failed to pass validation
-        # In the case of RolekitError, it is recommended to
-        # provide an explanation of the failure as the msg
-        # field of the exception.
-        # Example:
-        #   raise RolekitError(INVALID_VALUE,
-        #                      "{0} must be at least eight characters"
-        #                      .format(prop))
-
-        if prop in [ "cache_size" ]:
-            import psutil
-
-            self.check_type_int(value)
-            if value > psutil.virtual_memory().total / MiB_SIZE:
-                raise RolekitError(INVALID_VALUE,
-                                   "Cache size exceeds physical memory")
-            return True
-
-        elif prop in [ "connections" ]:
-            return self.check_type_int(value)
-
-        elif prop in [ "threads" ]:
-            self.check_type_int(value)
-            # Up to four threads should be safe on any platform
-            # More than that should be limited by the available CPUs
-            if value <= 4:
-                return True
-            elif value > os.cpu_count():
-                raise RolekitError(INVALID_VALUE,
-                                   "Number of threads exceeds available CPUs")
-            return True
-
-        # We didn't recognize this argument
-        return False
-
-
-    @staticmethod
-    def do_get_dbus_property(x, prop):
-        # This method tells rolekit what D-BUS type to use for each
-        # of this role's custom settings.
-
-        if prop in [ "connections",
-                     "threads" ]:
-            return dbus.Int32(x.get_property(x, prop))
-        elif prop in [ "cache_size" ]:
-            return dbus.Int64(x.get_property(x, prop))
-
-        # Lastly, always fall through to INVALID_PROPERTY if
-        # the setting is unknown.
-        raise RolekitError(INVALID_PROPERTY, prop)
